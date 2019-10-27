213cff528304da6a69dc916b884f5efd24e82fdb
SAİTCAN TAKILAN <saitcan.takilan@hotmail.com>
2019-10-27 16:30:56 +0300

    İş Katmanı ve İşlem Sonuçları Sistemi kodlandı. -SuccessDataResult altyapısı -ErrorDataResult altyapısı -İş Katmanının Refactor edilmesi -Magic Stringden kurtulması.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Utilities.Results;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        IDataResult<Product> GetById(int productId);
+        IDataResult<List<Product>> GeList();
+        IDataResult<List<Product>> GetListByCategory(int categoryId);
+
+        IResult Add(Product model);
+        IResult Delete(Product model);
+        IResult Update(Product model);
+    }
+}
---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public IDataResult<Product> GetById(int productId)
+        {
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
+        }
+
+        public IDataResult<List<Product>> GeList()
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetList().ToList());
+        }
+
+        public IDataResult<List<Product>> GetListByCategory(int categoryId)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetList(p => p.CategoryId == categoryId).ToList());
+        }
+
+        public IResult Add(Product model)
+        {
+            //Business codes
+            _productDal.Add(model);
+            return new SuccessResult(Messages.ProductAdded);
+        }
+
+        public IResult Delete(Product model)
+        {
+            _productDal.Delete(model);
+            return new SuccessResult(Messages.ProductDeleted);
+        }
+
+        public IResult Update(Product model)
+        {
+            _productDal.Update(model);
+            return new SuccessResult(Messages.ProductUpdated);
+        }
+    }
+}
